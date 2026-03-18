e123da75b590a6ea985d7b46784803f3d7e4b987
Junha Park <poiu7825@pusan.ac.kr>
2026-03-18 22:12:53 +0900

    feat: 제출 버튼 이벤트 동작 추가

--- js/index.js
+++ js/index.js
@@ -7,6 +7,39 @@ const tilList = document.querySelector("#til-list");
 
 tilForm.addEventListener("submit", function (event) {
   event.preventDefault();
+  
+  // 입력된 값을 가져오기
+  const date = document.getElementById('til-date').value;
+  const title = document.getElementById('til-title').value;
+  const content = document.getElementById('til-content').value;
 
-  // TODO: 입력값을 가져와서 새 TIL 항목을 만들어 목록에 추가하세요
+  // 새로운 리스트 아이템 생성
+  const listItem = document.createElement('li');
+  listItem.className = 'til-item';
+
+  // 내부에 들어갈 내용을 구성
+  listItem.innerHTML = `
+    <article class="item-header">
+      <time>[${date}]</time>
+      <h3>${title}</h3>
+      <button type="button" class="delete-btn">삭제</button>
+      <p>${content.replace(/\n/g, '<br>')}</p>
+    </article>
+    <hr>
+  `;
+
+  // 삭제 버튼을 찾아 이벤트를 붙인다.
+  const deleteBtn = listItem.querySelector('.delete-btn');
+  deleteBtn.addEventListener('click', function() {
+    if (confirm('정말 삭제하시겠습니까?')) {
+      // 해당 리스트 아이템을 화면에서 제거
+      listItem.remove();
+    }
+  });
+
+  // 목록의 아래에 항목 추가
+  tilList.append(listItem);
+
+  // 추가 후 폼 초기화
+  tilForm.reset();
 });
